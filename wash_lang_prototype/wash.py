fcae7e6f827fa222405af6a3ab018d4967e51ba1
Filip Ivković <fivkovic@uns.ac.rs>
2021-06-02 01:58:31 +0200

    Change API to support configuration options. Separate execution logic. #12
    - Changed API to support configuration options.
    - Moved execution logic to separate executor class.
    - Added support for different execution result types (python dict, JSON string).

--- wash_lang_prototype/wash.py
+++ wash_lang_prototype/wash.py
@@ -1,26 +1,27 @@
 import codecs
+import json
 import os
+from typing import Any
 
-from textx import metamodel_from_file
+from textx import metamodel_for_language
 
 from wash_lang_prototype.core.exceptions import WashError
+from wash_lang_prototype.core.executor import WashExecutor, create_executor_instance
+from wash_lang_prototype.core.options import WashOptions
 
 
 class Wash:
     def __init__(self, **kwargs):
-        self.__webdriver_path = kwargs.pop('webdriver_path')
-        self.__script = kwargs.pop('script')
-        self.__metamodel = kwargs.pop('metamodel')
-        self.__model = kwargs.pop('model')
-        self.__debug = kwargs.pop('debug')
+        self.__executor = kwargs.pop('executor')                    # type: WashExecutor
 
     @staticmethod
-    def from_file(webdriver_path: str, script_file_path: str, encoding='utf-8', debug=False, **kwargs):
+    def from_file(script_file_path: str, options: WashOptions, encoding: str = 'utf-8', debug=False, **kwargs):
         """
         Creates a new executable WASH instance.
+
         Args:
-            webdriver_path(str): The path of the WebDriver executable file.
             script_file_path(str): The path of the WASH script file.
+            options(WashOptions): Instance of WashOptions that enables configuration of the WASH executor.
             encoding(str): The encoding to be used for reading the contents of the WASH script file.
             debug(bool): Indicates whether debug messages should be printed or not.
         """
@@ -29,33 +30,60 @@ class Wash:
         with codecs.open(script_file_path, 'r', encoding) as script_file:
             script = script_file.read()
 
-        return Wash.from_string(webdriver_path=webdriver_path, script=script,
-                                encoding=encoding, debug=debug, **kwargs)
+        return Wash.from_string(script=script, options=options, encoding=encoding,
+                                script_file_path=script_file_path, debug=debug, **kwargs)
 
     @staticmethod
-    def from_string(webdriver_path: str, script: str, encoding='utf-8', debug=False, **kwargs):
+    def from_string(script: str, options: WashOptions, encoding: str = 'utf-8',
+                    script_file_path=None, debug=False, **kwargs):
         """
         Creates a new executable WASH instance.
+
         Args:
-            webdriver_path(str): The path of the WebDriver executable file.
             script(str): The contents of the WASH script.
+            options(WashOptions): Instance of WashOptions that enables configuration of the WASH executor.
             encoding(str): The encoding to be used for reading the contents of the WASH script file.
+            script_file_path(str): The path of the WASH script file.
             debug(bool): Indicates whether debug messages should be printed or not.
         """
 
         if type(script) is not str:
             raise WashError("Invalid script format: only UTF-8 encoded scripts are supported.")
 
-        return Wash.__create_instance(webdriver_path=webdriver_path, script=script,
-                                      encoding=encoding, debug=debug, **kwargs)
+        return Wash.__create_instance(script=script, options=options, encoding=encoding,
+                                      script_file_path=script_file_path, debug=debug, **kwargs)
 
     @staticmethod
-    def __create_instance(webdriver_path: str, script: str, encoding='utf-8', debug=False, **kwargs):
-        # TODO (fivkovic): Re-visit this to verify that it works as a package.
-        path_to_metamodel = os.path.join(os.path.dirname(__file__), 'lang', 'wash.tx')
+    def __create_instance(script: str, options: WashOptions, encoding: str = 'utf-8',
+                          script_file_path=None, debug=False, **kwargs):
+        """
+        Creates a new executable WASH instance.
 
-        metamodel = metamodel_from_file(path_to_metamodel, debug=debug)
-        model = metamodel.model_from_str(script, encoding=encoding, debug=debug)
+        Args:
+            script(str): The contents of the WASH script.
+            options(WashOptions): Instance of WashOptions that enables configuration of the WASH executor.
+            encoding(str): The encoding to be used for reading the contents of the WASH script file.
+            debug(bool): Indicates whether debug messages should be printed or not.
+        """
+        metamodel = metamodel_for_language('wash')
+        model = metamodel.model_from_str(script, encoding=encoding, file_name=script_file_path, debug=debug)
 
-        return Wash(**dict(kwargs, webdriver_path=webdriver_path, script=script,
-                           metamodel=metamodel, model=model, debug=debug))
+        # NOTE: Providing the filename as parameter is required as a workaround
+        # for using model_from_str having _tx_filename set at the same time.
+
+        executor = create_executor_instance(script=script, options=options, metamodel=metamodel,
+                                            model=model, debug=debug, **kwargs)
+
+        return Wash(executor=executor)
+
+    def execute(self) -> dict[str, Any]:
+        """
+        Executes the WASH script and returns the execution result in form of a dictionary.
+        """
+        return self.__executor.execute()
+
+    def execute_as_json(self) -> str:
+        """
+        Executes the WASH script and returns the execution result as a JSON string value.
+        """
+        return json.dumps(self.__executor.execute())
