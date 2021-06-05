b38954579521cf358687e50dbea4e67925fb6447
ktukelic <tukelic.katarina@gmail.com>
2021-06-05 19:18:42 +0200

    Add logic to executor module for starting a Chrome webdriver instance #41
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

--- wash_lang_prototype/core/executor.py
+++ wash_lang_prototype/core/executor.py
@@ -1,13 +1,21 @@
+import os
 from typing import Any
+
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 from textx.metamodel import TextXMetaModel
 
 from wash_lang_prototype.core.options import WashOptions
+
 
 def create_executor_instance(script: str, options: WashOptions, metamodel: TextXMetaModel,
                              model, debug=False, **kwargs):
 
     # TODO (fivkovic): Create executor based on current configuration.
     # Current configuration set in WASH script should contain what browser is to be used.
+
+    return ChromeExecutor(**dict(kwargs, script=script, options=options,
+                                 metamodel=metamodel, model=model, debug=debug))
 
 
 class WashExecutor:
@@ -20,3 +28,32 @@ class WashExecutor:
 
     def execute(self) -> dict[str, Any]:
         return {}
+
+    def _start_webdriver_instance(self, url: str) -> WebDriver:
+        pass
+
+
+
+class ChromeExecutor(WashExecutor):
+    """
+    WASH script executor that uses Chrome browser.
+    """
+    def __init__(self, **kwargs):
+        super(ChromeExecutor, self).__init__(**kwargs)
+
+    def _start_webdriver_instance(self, url: str):
+        from selenium.webdriver import ChromeOptions
+
+        options = ChromeOptions()
+        options.headless = True
+        options.add_argument("--window-size=1920,1080")
+
+        if not os.path.exists(self._options.chrome_webdriver_path):
+            raise FileNotFoundError('Unable to find Chrome WebDriver on specified path: "{}"'
+                                    .format(self._options.chrome_webdriver_path))
+
+        webdriver_instance = webdriver.Chrome(options=options, executable_path=self._options.chrome_webdriver_path)
+        webdriver_instance.get(url)
+
+        return webdriver_instance
+
