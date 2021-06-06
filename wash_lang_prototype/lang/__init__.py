239133b478592fc6d7140c80280d19ecb540fa39
Filip Ivković <fivkovic@uns.ac.rs>
2021-06-06 02:33:07 +0200

    Add internal component models to WASH metamodel using built-in model repository #42
    Co-Authored-By: Katarina Tukelić <tukelic.katarina@gmail.com>

--- wash_lang_prototype/lang/__init__.py
+++ wash_lang_prototype/lang/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from textx import language, metamodel_from_file
+from textx import language, metamodel_from_file, scoping, metamodel_for_language
 
 
 @language(name='wash', pattern='*.wash')
@@ -10,10 +10,22 @@ def wash_language():
     """
     from .wash import wash_classes
 
-    path_to_metamodel = os.path.join(os.path.dirname(__file__), 'wash.tx')
-    meta_model = metamodel_from_file(path_to_metamodel, classes=wash_classes)
+    wash_internal_meta_model = metamodel_for_language('wash_internal')
+    internal_folder = os.path.join(os.path.dirname(__file__), '..', 'internal')
 
-    
+    # NOTE: Refer to https://github.com/textX/textX/blob/master/textx/scoping/__init__.py#L35-L290
+    builtin_models_repository = scoping.ModelRepository()
+
+    for internal_file in os.listdir(internal_folder):
+        internal_file_model = wash_internal_meta_model.model_from_file(os.path.join(internal_folder, internal_file))
+        builtin_models_repository.add_model(internal_file_model)
+
+    path_to_metamodel = os.path.join(os.path.dirname(__file__), 'wash.tx')
+    meta_model = metamodel_from_file(path_to_metamodel,
+                                     classes=wash_classes,
+                                     builtin_models=builtin_models_repository, 
+                                     autokwd=True)
+
     return meta_model
 
 
@@ -30,6 +42,5 @@ def wash_internal_language(**kwargs):
 
     internal_folder = os.path.join(os.path.dirname(__file__), '..', 'internal')
     model = meta_model.model_from_file(os.path.join(internal_folder, 'configuration_options.washy'))
-
 
     return meta_model
