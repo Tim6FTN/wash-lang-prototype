fd39fd0920fb628ab63400d6caeb54def7e3e145
Marko131 <markostanic222@gmail.com>
2021-06-06 13:34:35 +0200

    Add class for selector by element name #25, #28
    Co-Authored-By: Filip Ivković <fivkovic@uns.ac.rs>

--- wash_lang_prototype/lang/wash.py
+++ wash_lang_prototype/lang/wash.py
@@ -67,6 +67,14 @@ class IDSelectorQuery(SelectorQuery):
         return execution_context.find_element_by_id(self.query_value.value)
 
 
+class NameSelectorQuery(SelectorQuery):
+    def __init__(self, parent, query_value):
+        super().__init__(parent, query_value)
+
+    def _execute(self, execution_context):
+        return execution_context.find_elements_by_name(self.query_value.value)
+
+
 class CSSSelectorQuery(SelectorQuery):
     def __init__(self, parent, query_value):
         super().__init__(parent, query_value)
