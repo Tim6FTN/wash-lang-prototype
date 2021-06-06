35d22c7912aecc5e406e7138059038006ba9c30a
Filip Ivković <fivkovic@uns.ac.rs>
2021-06-06 02:18:58 +0200

    Register wash_internal language #42, #50
    Co-Authored-By: Katarina Tukelić <tukelic.katarina@gmail.com>

--- wash_lang_prototype/lang/__init__.py
+++ wash_lang_prototype/lang/__init__.py
@@ -12,4 +12,24 @@ def wash_language():
 
     path_to_metamodel = os.path.join(os.path.dirname(__file__), 'wash.tx')
     meta_model = metamodel_from_file(path_to_metamodel, classes=wash_classes)
+
+    
+    return meta_model
+
+
+
+@language(name='wash_internal', pattern='*.washy')
+def wash_internal_language(**kwargs):
+    """
+    WASH Internal DSL - domain specific language used internally to extend WASH.
+    """
+    from .wash_internal import internal_classes
+
+    path_to_metamodel = os.path.join(os.path.dirname(__file__), 'wash_internal.tx')
+    meta_model = metamodel_from_file(path_to_metamodel, classes=internal_classes, autokwd=True, **kwargs)
+
+    internal_folder = os.path.join(os.path.dirname(__file__), '..', 'internal')
+    model = meta_model.model_from_file(os.path.join(internal_folder, 'configuration_options.washy'))
+
+
     return meta_model
