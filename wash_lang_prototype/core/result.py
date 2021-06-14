e0cb2a448a3b1c229b554808bb170480e461a0e8
Filip Ivković <fivkovic@uns.ac.rs>
2021-06-14 22:49:42 +0200

    Fix JSON serialization issue in ExecutionResult class. Add comments. #67
    Co-Authored-By: Katarina Tukelić <tukelic.katarina@gmail.com>
    Co-Authored-By: m-milovanovic <39669371+m-milovanovic@users.noreply.github.com>
    Co-Authored-By: mdjokic <41081275+mdjokic@users.noreply.github.com>
    Co-Authored-By: Marko131 <18128261+Marko131@users.noreply.github.com>

--- wash_lang_prototype/core/result.py
+++ wash_lang_prototype/core/result.py
@@ -13,6 +13,9 @@ class ExecutionResult:
         return str(self.__get_dict())
 
     def __get_dict(self):
+        """
+        Returns the attribute dictionary of the object with omitted private attributes.
+        """
         attributes_dictionary = self.__dict__.copy()
         attributes_dictionary.pop('_ExecutionResult__parent')
 
@@ -21,8 +24,8 @@ class ExecutionResult:
     def add_attributes(self, **kwargs):
         """
         Adds given attributes to the instance.
-        In case the instance already has a given attribute, it will update the existing attribute
-        by either adding new items to it, or replace the present value.
+        In case the instance already has a given attribute, it will update the existing attribute value
+        by either adding new items to it, or replace the present values.
         """
         for attr_key, attr_value in kwargs.items():
             if not hasattr(self, attr_key):
@@ -30,15 +33,17 @@ class ExecutionResult:
             else:
                 attribute = getattr(self, attr_key)
                 if isinstance(attribute, ExecutionResult):
-                    new_attributes = {k: attr_value.__dict__[k] for k in attr_value.__dict__ if k != '__parent'}
+                    new_attributes = {k: attr_value.__dict__[k] for k
+                                      in attr_value.__dict__ if k != '_ExecutionResult__parent'}
                     attribute.add_attributes(**new_attributes)
                 elif isinstance(attribute, list):
                     # TODO (fivkovic): Handle case when len not equal
                     for new_item, existing_item in zip(attr_value, attribute):
-                        new_attributes = {k: new_item.__dict__[k] for k in new_item.__dict__ if k != '__parent'}
+                        new_attributes = {k: new_item.__dict__[k] for k
+                                          in new_item.__dict__ if k != '_ExecutionResult__parent'}
                         existing_item.add_attributes(**new_attributes)
                 else:
                     attribute = attr_value
 
     def to_json(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
+        return json.dumps(self, default=lambda o: o.__get_dict())
