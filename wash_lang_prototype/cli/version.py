662c4fdbc457fdce06df20a91f95042ea2084b62
Marko131 <markostanic222@gmail.com>
2021-06-14 22:38:22 +0200

    Update version CLI command #68

--- wash_lang_prototype/cli/version.py
+++ wash_lang_prototype/cli/version.py
@@ -1,11 +1,15 @@
 try:
     import click
 except ImportError:
-    raise Exception('Missing CLI dependencies')
+    raise Exception('Missing CLI dependencies. To use WASH from CLI, please run following command:/n'
+                    'pip install wash-lang-prototype[cli]')
 
 
 def version(wash_lang_prototype):
     @wash_lang_prototype.command()
     def version():
+        """
+        Displays the WASH package version.
+        """
         import wash_lang_prototype
         click.echo('wash_lang_prototype {}'.format(wash_lang_prototype.__version__))
